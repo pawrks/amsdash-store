2ba482c0b7b0def3362d49caa4ecbb981396cc5b
parker <prwilliams5@gmail.com>
2023-09-01 17:10:57 -0500

    updated main navbar logo and sizing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
+import { IoIosPulse } from "react-icons/io";
 
 const Navbar = async () => {
   const categories = await getCategories();
@@ -13,7 +14,8 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">CloudKey | Colo</p>
+            <p className="font-bold text-2xl">Pulse Rack</p>
+            <IoIosPulse className="mr-2 h-9 w-9" />
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
